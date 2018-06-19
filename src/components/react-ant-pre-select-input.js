7f746a016706cd36449363c9773ef5e73eac23c8
afeiship <1290657123@qq.com>
2018-06-19 19:12:16 +0800

    issue: onClear can not trigger before onChange

--- src/components/react-ant-pre-select-input.js
+++ src/components/react-ant-pre-select-input.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import noop from 'noop';
@@ -7,13 +7,14 @@ import ReactAntSelect from 'react-ant-select';
 import nx from 'next-js-core2';
 
 
-export default class extends Component{
+export default class extends Component {
   /*===properties start===*/
   static propTypes = {
     className: PropTypes.string,
     items: PropTypes.array,
     value: PropTypes.array,
     onChange: PropTypes.func,
+    onClear: PropTypes.func,
     eventValue: PropTypes.func,
     Component: PropTypes.func,
   };
@@ -22,55 +23,60 @@ export default class extends Component{
     items: [],
     value: [],
     onChange: noop,
+    onClear: noop,
     Component: Input,
     eventValue: nx.returnValue
   };
   /*===properties end===*/
 
-  constructor(inProps){
+  constructor(inProps) {
     super(inProps);
     this.state = {
       value: inProps.value
     }
   }
 
-  componentWillReceiveProps(inProps){
+  componentWillReceiveProps(inProps) {
     const { value } = inProps;
-    if( value !== this.state.value ){
+    if (value !== this.state.value) {
       this.setState({ value });
     }
   }
 
-  get selectView(){
+  get selectView() {
     const { items, value } = this.props;
     return (
       <ReactAntSelect
         items={items}
-        style={{width:120}}
+        style={{ width: 120 }}
         value={[value[0]]}
         onChange={this._onChange.bind(this, 0)}
-        />
+      />
     );
   }
 
   _onChange = (inIndex, inEvent) => {
-    const { value} = this.props;
-    const { onChange, eventValue } = this.props;
+    const { value } = this.props;
+    const { onClear, onChange, eventValue } = this.props;
     value[inIndex] = inEvent.target.value;
-    this.setState({ value },()=>{
+    this.setState({ value }, () => {
       onChange({
         target: { value: eventValue(value) }
       });
+      !value[inIndex] && onClear();
     });
   };
 
-  render(){
-    const { className, onChange, Component, eventValue, items, value, ...props } = this.props;
+
+
+  render() {
+    const { className, onChange, onClear, Component, eventValue, items, value, ...props } = this.props;
     return (
       <Component
         addonBefore={this.selectView}
         value={value[1]}
-        onChange={this._onChange.bind(this,1)}
+        onChange={this._onChange.bind(this, 1)}
+        onClear={this._onChange.bind(this, 1)}
         className={classNames('react-ant-pre-select-input', className)}
         {...props} />
     );
